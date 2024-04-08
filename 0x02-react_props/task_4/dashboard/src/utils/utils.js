730ac113da7c889d09c3e8361850e7f48620ee19
M-arsden <marsden.ke@gmail.com>
2024-04-08 18:14:01 +0300

    velo

---
+++ 0x02-react_props/task_4/dashboard/src/utils/utils.js
@@ -0,0 +1,28 @@
+export const getFullYear = () => {
+	rt const getFullYear = () => {
+		  return new Date().getFullYear();
+	};
+
+	export const getFooterCopy = (isIndex) => {
+		  let res = undefined;
+		  isIndex
+		    ? (res = 'Holberton School')
+		    : (res = 'Holberton School main dashboard');
+		  return res;
+	};
+
+	export const getLatestNotification = () =>
+		  '<strong>Urgent requirement</strong> - complete by EOD';
+  return new Date().getFullYear();
+};
+
+export const getFooterCopy = (isIndex) => {
+  let res = undefined;
+  isIndex
+    ? (res = 'Holberton School')
+    : (res = 'Holberton School main dashboard');
+  return res;
+};
+
+export const getLatestNotification = () =>
+  '<strong>Urgent requirement</strong> - complete by EOD';
